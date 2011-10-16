c5cf9b3f024354f11ce800e8891ce16e7a2bcc74
Jean-Philippe Sirois <email@jpsirois.com>
2011-10-16 15:06:10 -0400

    Styling de la Google maps du footer pour la rendre plus aux couleurs du WAQ

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,13 +1,60 @@
 function initialize() {
   var latlng = new google.maps.LatLng(46.81303, -71.22538);
-  var myOptions = {
+
+  var map = new google.maps.Map(document.getElementById("googlemaps-footer"), {
     zoom: 15,
     center: latlng,
     scrollwheel: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true
-  };
-  var map = new google.maps.Map(document.getElementById("googlemaps-footer"), myOptions);
+    disableDefaultUI: true,
+    mapTypeControl: false,
+    navigationControl: true,
+    mapTypeControlOptions: {
+      mapTypeIds: 'waq'
+    }
+  });
+
+  /* Styles {{{ */
+
+  var mapStyles = [
+  {
+    featureType: "landscape",
+      elementType: "all",
+      stylers: [
+      { hue: "#000000" },
+      { saturation: -255 },
+      { lightness: 3 }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+    { hue: "#4CABCF" }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+    { hue: "#006D96" },
+    { lightness: 10 }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+    { hue: "#006D96" },
+    { saturation: -30 },
+    { gamma: 0.7 }
+    ]
+  }
+  ];
+  var styledMapType = new google.maps.StyledMapType(mapStyles, {name: 'waq'});
+  map.mapTypes.set('waq', styledMapType);
+  map.setMapTypeId('waq');
+
+  /*}}}*/
 
   var marker = new google.maps.Marker({
     position: latlng,
