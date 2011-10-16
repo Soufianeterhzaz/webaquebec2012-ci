336e0448896e829259245db2efa9772264fd7cbf
Jean-Philippe Veilleux <jpveilleux@pc12.webtelecom.local>
2011-10-16 03:04:27 -0400

    75% de l'accueil et du frame du site

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,105 @@
-Home page
+<div class="page-content-block clearfix">
+
+  <div class="left-side main">
+    <div id="vox-pop" class="white-box">
+      <div class="thumb">
+        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
+        <img src="assets/img/vox-pop-preview.jpg" alt="Apercu du Vox Pop">
+      </div>
+      <div class="content">
+        <h3>Random Vox Pop Title</h3>
+        <p>Aliquam posuere purus suscipit tortor accumsan et pretium nunc elementum. Ut neque dui, suscipit eget condimentum eget, vulputate vitae libero. Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius </p>
+
+      </div>
+      <ul class="social-bar list-custom list-hori clearfix">
+        <li><span>Suivez-nous</span></li>
+        <li><a class="btn-facebook" href="#" title="Suivez-nous sur Facebook">Facebook</a></li>
+        <li><a class="btn-twitter" href="#" title="Suivex-nous sur Twitter">Twitter</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="right-side main">
+    <div id="important-numbers">
+      <a id="btn-precedent" class="btn-nav" href="#"><span>Précédent</span></a>
+      <a id="btn-suivant" class="btn-nav" href="#"><span>Suivant</span></a>
+      <ul id="numbers-viewport" class="list-custom list-hori clearfix">
+        <li>
+          <span class="number">5</span>
+          <h2>Conérenciers Internationaux</h2>
+        </li>
+        <li>
+          <span class="number">25</span>
+          <h2>Conérenciers<br>Locaux</h2>
+        </li>
+        <li>
+          <span class="number">48</span>
+          <h2>Heures de<br>compétitions</h2>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</div>
+<div class="sep-shadow"></div>
+<div class="page-content-block">
+
+  <div class="left-side main">
+    <div id="news-box">
+      <h3 class="box-header">Dernières nouvelles</h3>
+      <ul class="list-custom">
+        <li>
+          <p>
+            Suspendisse potenti. Morbi ac enim quis nisi varius condime
+ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
+Proin et varius leo.<a href="#">Lire plus</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            Suspendisse potenti. Morbi ac enim quis nisi varius condime
+ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
+Proin et varius leo.<a href="#">Lire plus</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            Suspendisse potenti. Morbi ac enim quis nisi varius condime
+ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
+Proin et varius leo.<a href="#">Lire plus</a>
+          </p>
+        </li>
+      </ul>
+    </div>
+
+    <div id="newsletter-box">
+      <h3>Recevez les dernières nouvelles</h3>
+      <form name="newsletter" id="newsletter" method="post" action="#">
+        <input id="email" type="text" name="email" value="Votre courriel">
+        <button id="btn-submit-nl">Abonnez-vous</button>
+      </form>
+    </div>
+
+  </div>
+
+  <div class="right-side main">
+    <div class="left-side rs-content-box">
+      <h3 class="box-header">Iron Web</h3>
+      <div class="white-thumb">
+        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
+      </div>
+      <p>Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius leo. </p>
+    </div>
+    <div class="right-side rs-content-box">
+      <h3 class="box-header">Commanditaires principaux</h3>
+      <div class="white-thumb">
+        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
+      </div>
+      <p>Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius leo. </p>
+    </div>
+  </div>
+
+</div>
+
+
+
--- application/views/layouts/application_layout.php
+++ application/views/layouts/application_layout.php
@@ -3,65 +3,138 @@
 <!--[if IE 8 ]><html class="ie8" lang="fr"><![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html lang="fr"><!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  
-  <title>Web à Québec</title>
-  
-  <link rel="icon" href="<?php echo base_url(); ?>favicon.ico" />
-  
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css" />
-  
-  <?php if (ENVIRONMENT == 'production'): ?>
-  <script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Web à Québec</title>
+
+    <link rel="icon" href="<?php echo base_url(); ?>favicon.ico" />
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
+
+    <?php if (ENVIRONMENT == 'production'): ?>
+    <script>
     /*
       TODO CHANGE GA ACCOUNT ID
     */
     var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'XXXXXXXXXXXXXXXXXXXXXX']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
-  <?php endif ?>
-  
-  <script src="<?php echo base_url(); ?>assets/js/libs/modernizr-html5-shim.js"></script>
+        _gaq.push(['_setAccount', 'XXXXXXXXXXXXXXXXXXXXXX']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+    <?php endif ?>
+
+    <script src="<?php echo base_url(); ?>assets/js/libs/modernizr-html5-shim.js"></script>
   
 </head>
 
 <body>
-  <div id="page">
-    <header>
-    
+  <div class="ow">
+
+    <div id="page">
+
+      <?php
+      //###################################################################
+      //  HEADER   ########################################################
+      ?>
+      <header>
+        <div class="wrapper clearfix">
+
+          <h1 class="logo-princ"><span class="logo-accronym t-indent">WAQ</span><span class="logo-full-text to-caps">Le Web à Québec</span></h1>
+
+          <div class="event-details">
+            <span class="lieu">Espace 400e Bell. Québec</span>
+            <time class="dates" datetime="2012-02-22">22-25 Février 2012</time>
+            <a class="btn-inscription t-indent" href="#">Inscription
+              <img class="btn-insc-normal" src="assets/img/btn-insc.png" alt="Inscription">
+              <img class="btn-insc-hover" src="assets/img/btn-insc-hover.png" alt="Inscription">
+            </a>
+          </div>
+
+          <nav class="top-right">
+            <ul class="list-custom list-hori">
+              <li><a href="#">À propos</a></li>
+              <li><a href="#">Médias</a></li>
+              <li><a href="#">Partenaires</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+
+          <nav class="menu-princ">
+            <ul class="list-custom">
+              <li><a href="#">Horaire et programmation</a></li>
+              <li><a href="#">Compétition Iron Web</a></li>
+              <li><a href="#">Information pratique</a></li>
+            </ul>
+          </nav>
+
+        </div>
+      </header>
+
+      <?php 
+      //###################################################################
+      //  MAIN CONTENT   ##################################################
+      ?>
+
+      <div id="page-content">
+        <div class="wrapper clearfix">
+          <?php if(isset($page_content)): ?>
+
+          <?php echo $page_content; ?>
+
+          <?php endif; ?>
+        </div>
+      </div>
+
+    </div>
+
+    <?php
+    //###################################################################
+    //  FOOTER   ########################################################
+    ?>
+
+    <footer>
       <div class="wrapper clearfix">
-        WAQ
-      </div>  
-      
-    </header>
-    
-    <div id="page-content">
-      
-      <?php if(isset($page_content)): ?>
-      
-        <?php echo $page_content; ?>
-      
-      <?php endif; ?>
-      
-    </div>
-    
+        <div class="google-map"></div>
+        <h2 class="t-indent">WAQ</h2>
+        <address id="main-address">
+          <strong>Adresse de l'événement</strong>
+          100 rue Quai St-André<br>
+          Québec (Qc) G1K 3Y2
+        </address>
+        <div class="footer-box">
+          <address>
+            <strong>Siège social</strong>
+            305 Charest Est, bureau 901<br>
+            Québec (Qc) G1K 3H3<br>
+            418 647-3877
+          </address>
+        </div>
+        <div class="footer-box">
+          <ul class="list-custom">
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Médias</a></li>
+            <li><a href="#">Partenaires</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="footer-box">
+          <ul class="list-custom">
+            <li><a href="#">Horaire et programmation</a></li>
+            <li><a href="#">Compétition Iron Web</a></li>
+            <li><a href="#">Information pratique</a></li>
+            <li><a href="#">Inscription</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
   </div>
-  
-  <footer>
-    
-    <div class="wrapper clearfix">
-      
-    </div>
-    
-  </footer>
-  
+
 </body>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
