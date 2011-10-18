90f297165ed888aa6c42dd804f579c154129e29e
Jean-Philippe Sirois <email@jpsirois.com>
2011-10-17 23:27:46 -0400

    Ajustement du formulaire d'enregistrement a la newsletter pour etre linké au nouveau compte de campain monitor

--- application/views/contact.php
+++ application/views/contact.php
@@ -74,8 +74,8 @@ Proin et varius leo.<a href="#">Lire plus</a>
 
     <div id="newsletter-box">
       <h3>Recevez les dernières nouvelles</h3>
-      <form name="newsletter" id="newsletter" method="post" action="http://ix.createsend.com/t/y/s/uiluti/" class="clearfix">
-        <input id="email" type="text" name="cm-uiluti-uiluti" placeholder="Votre courriel">
+      <form name="newsletter" id="newsletter" method="post" action="http://webaquebec.createsend.com/t/r/s/akkjhl/" class="clearfix">
+        <input id="email" type="text" name="cm-akkjhl-akkjhl" placeholder="Votre courriel">
         <button id="btn-submit-nl" class="btn">M’abonner</button>
       </form>
     </div>
