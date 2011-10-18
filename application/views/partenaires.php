3b96f069803cef7693f1ecc4a0739cf9886d043f
Jean-Philippe Sirois <email@jpsirois.com>
2011-10-18 16:13:05 -0400

    Ajustements RUSH pour mise en ligne V3

--- application/views/partenaires.php
+++ application/views/partenaires.php
@@ -12,10 +12,27 @@
   <div class="sep-shadow"></div>
   <div class="page-content-block clearfix">
 
+    <h3 class="box-header">Commanditaires</h3>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/quebec-international.png" alt="Québec International"></li>
+    </ul>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/mirego.png" alt="Mirego"></li>
+    </ul>
+
+  </div>
+  <div class="sep-shadow"></div>
+  <div class="page-content-block clearfix">
+
     <h3 class="box-header">Partenaires publics</h3>
 
     <ul class="list-custom list-hori">
       <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/cldqc.png" alt="Centre local de développement de Québec"></li>
+    </ul>
+
+		<ul class="list-custom list-hori">
       <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/cre.png" alt="Conférence régionale des élus"></li>
     </ul>
 
@@ -26,8 +43,13 @@
     <h3 class="box-header">Partenaires de services</h3>
 
     <ul class="list-custom list-hori">
-      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/zapqc.png" alt="ZAP Québec"></li>
+			<li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/zapqc.png" alt="ZAP Québec"></li>
+		</ul>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/fernandez-communication.png" alt="Fernandez Communication"></li>
       <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/savori.png" alt="Savori"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/200x89/catherine-morissette.png" alt="Catherine Morissette"></li>
     </ul>
 
   </div>
