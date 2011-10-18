e1739dcbbce512be4aec1939ff64481ab8790397
Jean-Philippe Veilleux <jpveilleux@idealtechnology.net>
2011-10-18 00:52:14 -0400

    Ajout de la page partenaires et des logos

--- application/views/partenaires.php
+++ application/views/partenaires.php
@@ -1,105 +1,47 @@
-<div class="page-content-block clearfix">
+<div class="p-wrapper">
+  <div class="page-content-block clearfix">
 
-  <div class="left-side main">
-    <div id="vox-pop" class="white-box">
-      <div class="thumb">
-        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
-        <img src="assets/img/vox-pop-preview.jpg" alt="Apercu du Vox Pop">
-      </div>
-      <div class="content">
-        <h3>Random Vox Pop Title</h3>
-        <p>Aliquam posuere purus suscipit tortor accumsan et pretium nunc elementum. Ut neque dui, suscipit eget condimentum eget, vulputate vitae libero. Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius </p>
+    <h3 class="box-header">Commanditaires principaux</h3>
 
-      </div>
-      <ul class="social-bar list-custom list-hori clearfix">
-        <li><span>Suivez-nous</span></li>
-        <li><a class="btn-facebook" href="#" title="Suivez-nous sur Facebook">Facebook</a></li>
-        <li><a class="btn-twitter" href="#" title="Suivex-nous sur Twitter">Twitter</a></li>
-      </ul>
-    </div>
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/libeo.png" alt="Libeo"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/ixmedia.png" alt="iXmédia"></li>
+    </ul>
+
   </div>
+  <div class="sep-shadow"></div>
+  <div class="page-content-block clearfix">
 
-  <div class="right-side main">
-    <div id="important-numbers">
-      <a id="btn-precedent" class="btn-nav" href="#"><span>Précédent</span></a>
-      <a id="btn-suivant" class="btn-nav" href="#"><span>Suivant</span></a>
-      <ul id="numbers-viewport" class="list-custom list-hori clearfix">
-        <li>
-          <span class="number">5</span>
-          <h2>Conérenciers Internationaux</h2>
-        </li>
-        <li>
-          <span class="number">25</span>
-          <h2>Conérenciers<br>Locaux</h2>
-        </li>
-        <li>
-          <span class="number">48</span>
-          <h2>Heures de<br>compétitions</h2>
-        </li>
-      </ul>
-    </div>
+    <h3 class="box-header">Partenaires publics</h3>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/cldqc.png" alt="Centre local de développement de Québec"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/cre.png" alt="Conférence régionale des élus"></li>
+    </ul>
+
   </div>
+  <div class="sep-shadow"></div>
+  <div class="page-content-block clearfix">
 
+    <h3 class="box-header">Partenaires de services</h3>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/zapqc.png" alt="ZAP Québec"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/savori.png" alt="Savori"></li>
+    </ul>
+
+  </div>
+  <div class="sep-shadow"></div>
+  <div class="page-content-block clearfix">
+
+    <h3 class="box-header">Supporteurs</h3>
+
+    <ul class="list-custom list-hori">
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/qcmedia.png" alt="QCmédias"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/kronoshori.png" alt="Kronos"></li>
+      <li><img src="<?php echo base_url() ?>/assets/img/partenaires/umen.png" alt="UMEN"></li>
+    </ul>
+
+  </div>
 </div>
-<div class="sep-shadow"></div>
-<div class="page-content-block">
-
-  <div class="left-side main">
-    <div id="news-box">
-      <h3 class="box-header">Dernières nouvelles</h3>
-      <ul class="list-custom">
-        <li>
-          <p>
-            Suspendisse potenti. Morbi ac enim quis nisi varius condime
-ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
-Proin et varius leo.<a href="#">Lire plus</a>
-          </p>
-        </li>
-        <li>
-          <p>
-            Suspendisse potenti. Morbi ac enim quis nisi varius condime
-ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
-Proin et varius leo.<a href="#">Lire plus</a>
-          </p>
-        </li>
-        <li>
-          <p>
-            Suspendisse potenti. Morbi ac enim quis nisi varius condime
-ntum in at sapien. Ut vitae tortor nunc, quis feugiat dolor.
-Proin et varius leo.<a href="#">Lire plus</a>
-          </p>
-        </li>
-      </ul>
-    </div>
-
-    <div id="newsletter-box">
-      <h3>Recevez les dernières nouvelles</h3>
-      <form name="newsletter" id="newsletter" method="post" action="http://webaquebec.createsend.com/t/r/s/akkjhl/" class="clearfix">
-        <input id="email" type="text" name="cm-akkjhl-akkjhl" placeholder="Votre courriel">
-        <button id="btn-submit-nl" class="btn">M’abonner</button>
-      </form>
-    </div>
-
-  </div>
-
-  <div class="right-side main">
-    <div class="left-side rs-content-box">
-      <h3 class="box-header">Iron Web</h3>
-      <div class="white-thumb">
-        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
-      </div>
-      <p>Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius leo. </p>
-    </div>
-    <div class="right-side rs-content-box">
-      <h3 class="box-header">Commanditaires principaux</h3>
-      <div class="white-thumb">
-        <a href="#">Écoutez<strong>le Vox Pop</strong></a>
-      </div>
-      <p>Suspendisse potenti. Morbi ac enim quis nisi varius condimentum in at sapien. Ut vitae tortor nunc, quis feugiat dolor. Proin et varius leo. </p>
-    </div>
-  </div>
-
-</div>
-
-
 
