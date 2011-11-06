5b21c810e9882e2d2e74e52066c2990e74986594
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2011-11-06 18:52:29 -0500

    Iron Web : remixed header

--- application/views/iron_web.php
+++ application/views/iron_web.php
@@ -1,42 +1,30 @@
 <div class="ros-wrapper">
-  <div id="iw-header" class="page-content-block clearfix">
-    <div id="logo-iw">
-      <h1 class="t-indent">Iron Web</h1>
-      <span class="to-cap"></span>
-    </div>
-
-    <a id="btn-candidature" href="<?php echo base_url() ?>iron-web-inscriptions/">
-      <span class="call-to-action">Soumettre votre candidature</span>
-      <span class="date-limite">Date limite: 1 décembre</span>
-    </a>
-		
-  </div>
-  <div class="sep-shadow"></div>
   <div class="page-content-block clearfix">
-
-    <div class="left-side main">
+    <div id="iw-header" class="left-side main">
+      <div id="logo-iw">
+        <h1 class="t-indent">Iron Web</h1>
+        <span class="to-cap"></span>
+      </div>
+    
+      <div class="rs-content-box">
+        <p>Le Iron Web est une compétition “Web réalité” pour les passionnés du Web qui désirent participer au projet le plus court mais le plus intense de leur vie. Confinés à l’Espace 400e Bell pendant 48 heures, les participants devront concevoir un site Web rien de moins qu’extraordinaire. Une réalisation qui épatera le jury et le public, mais surtout, qui permettra à Québec de rayonner comme joueur important du Web au Québec et même dans le monde entier!</p>
+      </div>
+      
+      <div class="clearfix"></div>
+      <a id="btn-candidature" href="<?php echo base_url() ?>iron-web-inscriptions/">
+        <span class="call-to-action">Soumettre votre candidature</span>
+        <span class="date-limite">Date limite: 1 décembre</span>
+      </a>
     </div>
-
-    <article class="right-side main">
-      <div class="left-side rs-content-box">
-        <h3 class="box-header">48 heures de compétition</h3>
-        <p>Le Iron Web est une compétition pour les passionnés du Web qui désirent participer au projet le plus court mais le plus intense de leur vie. Confinés à l’Espace 400e Bell pendant 48 heures, les participants devront concevoir un site Web rien de moins qu’extraordinaire. Une réalisation qui épatera le jury et le public, mais surtout, qui permettra à Québec de rayonner comme joueur important du Web au Québec et même dans le monde entier!</p>
-      </div>
-
-      <div class="right-side rs-content-box">
-        <h3 class="box-header">Web diffusion HD</h3>
-        <p>Durant 3 jours, vous aurez l’opportunité d’observer en direct, depuis le confort de votre foyer, une équipe se créer et un projet se bâtir. Équipés de matériel audiovisuel de haute qualité, les lieux de l’Espace 400e deviendront le théâtre d’une réalisation Web unique. Plus qu’une simple diffusion, participants et spectateurs seront invités à interagir tout au long de leur périple.</p>
-      </div>
-    </article>
     <div class="clearfix"></div>
+    <div class="sep-shadow"></div>
     <div id="iw-avantages" class="rs-content-box">
       <h3 class="box-header">Participer c'est</h3>
       <ul class="list-custom">
-        <li>Une visibilité accru auprès de 500 personnes</li>
         <li>Du réseautage avec des professionnels reconnus</li>
         <li>Une occasion d'apprentissage hors-pair</li>
-        <li>Une visibilité accrue pour montrer votre savoir-faire</li>
         <li>Participez à une conférence de votre choix durant le WAQ</li>
+        <li>Une visibilité accru auprès de 500 personnes</li>
         <li>Vous voir sur internet pendant 48 heures</li>
         <li>Sortir de votre zone de confort!</li>
       </ul>
