723d006fb36da132ba31ecae7ae733a822a52c33
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2011-11-09 23:00:14 -0500

    CSS filename version (trying to avoid pagespeed cache)

--- application/views/layouts/application_layout.php
+++ application/views/layouts/application_layout.php
@@ -10,7 +10,7 @@
 
     <link rel="icon" href="<?php echo base_url(); ?>favicon.ico" />
 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style-2.css" />
 
     <script src="<?php echo base_url(); ?>assets/js/libs/modernizr-html5-shim.js"></script>
   
