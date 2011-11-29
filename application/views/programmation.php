2773213b61d260ee269acad66fa0a08412a56bdd
Jean-Philippe Veilleux <jpveilleux@idealtechnology.net>
2011-11-28 22:13:55 -0500

    Ajout de prog test

--- application/views/programmation.php
+++ application/views/programmation.php
@@ -22,18 +22,85 @@
     <h3 class="box-header">Conférenciers du Québec</h3>
 
     <ul class="list-custom list-hori">
-        <li>
-          <a target="blank" href="http://www.quebecinternational.ca/">
-            <span>Peter Morville</span>
-            <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
-          </a>
-        </li>
+      <li>
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+
       <li class="last">
         <a target="blank" href="http://www.quebecinternational.ca/">
           <span>Peter Morville</span>
           <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
         </a>
       </li>
+
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+
+    </ul>
+    <ul class="list-custom list-hori">
+
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+
     </ul>
 
   </div>
