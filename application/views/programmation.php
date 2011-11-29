06fa7c351096d71a88747957e0479953a553185d
Jean-Philippe Veilleux <jpveilleux@idealtechnology.net>
2011-11-28 21:47:33 -0500

    Création et styling de base de la section programmation

---
+++ application/views/programmation.php
@@ -0,0 +1,41 @@
+<div class="prog-wrapper">
+
+  <div class="page-content-block clearfix">
+
+    <h3 class="box-header">Commanditaires internationaux</h3>
+
+    <ul class="list-custom list-hori">
+      <li>
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+    </ul>
+
+  </div>
+
+  <div class="sep-shadow"></div>
+
+  <div class="page-content-block clearfix">
+
+    <h3 class="box-header">Conférenciers du Québec</h3>
+
+    <ul class="list-custom list-hori">
+        <li>
+          <a target="blank" href="http://www.quebecinternational.ca/">
+            <span>Peter Morville</span>
+            <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+          </a>
+        </li>
+      <li class="last">
+        <a target="blank" href="http://www.quebecinternational.ca/">
+          <span>Peter Morville</span>
+          <img src="<?php echo base_url() ?>assets/img/conferenciers/147x160/peter-morville.jpg" alt="Québec International">
+        </a>
+      </li>
+    </ul>
+
+  </div>
+</div>
+
